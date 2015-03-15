cfe4374ded73f898f779dd5c653d5f2e32ea0deb
Artamoshkin Maxim <zverit@gmail.com>
2015-03-15 19:48:44 +0300

    no message

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 var express = require('express');
+var expressSession = require('express-session')
 var path = require('path');
 var favicon = require('serve-favicon');
+var mongoose = require('mongoose');
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
+
 var users = require('./routes/users');
 
 
 var app = express();
-
-
-
-var Cat = mongoose.model('Cat', { name: String });
-
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-    if (err) // ...
-        console.log('meow');
+var User = require('./models/user').User;
+var user = new User({
+    login: "Vasa",
+    password: "pass"
 });
+
+user.save(function(err, user, affacted){
+ console.log(user);
+ var user = new User({
+ login: "Not",
+ password: "passed"
+ });
+ user.save(function(err, user, affacted){
+ console.log(user);
+ });
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('ejs', require('ejs-locals'));
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -31,10 +42,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+var MongoStore = require('connect-mongo')(expressSession);
+app.use(expressSession({
+    secret: "secret",
+    key : "sid",
+    cookie:{
+        "path" : "/",
+        "httpOnly" : true,
+        "maxAge" : null
+    },
+    store : new MongoStore({mongoose_connection: mongoose.connection})
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+require('./routes/index')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var User = require('../models/user').User;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(app) {
+    app.get('/', function (req, res, next) {
+        res.render('index', {title: 'Express'});
+    });
 
-router.get('/admin', function(req, res, next) {
-    res.render('index', { title: 'Admin' });
-});
+    app.get('/admin', function (req, res, next) {
+        res.render('index', {title: 'Admin'});
+    });
 
-module.exports = router;
+    app.get("/users", function (req, res, next) {
+        User.find({}, function (err, users) {
+            if (err) return next(err);
+            res.json(users);
+        })
+    });
+
+    app.get("/user:id", function (req, res, next) {
+        User.findById(req.params.id, function (err, user) {
+            if (err) return next(err);
+            res.json(user);
+        })
+    });
+};
